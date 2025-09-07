ba4fb0c808901fd4b395afc86108a13f16c2625a
zmigd <zmigdalovich@gmail.com>
2025-09-07 19:34:45 +0300

    rename APP

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -2,7 +2,7 @@ import css from './Notification.module.css';
 import React from 'react';
 
 const Notification: React.FC = () => {
-  return <p className={css.message}>No feedback yet</p>;
+  return <p className={css.message}>No feedback ye</p>;
 };
 
 export default Notification;
