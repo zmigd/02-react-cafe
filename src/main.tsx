9d2f7c111622825f7d71cf34c2e040034ba10d26
zmigd <zmigdalovich@gmail.com>
2025-09-07 19:19:03 +0300

    change App

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/APP/App';
+import App from './components/App/App';
 import 'modern-normalize'; 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
