c03139c7826107c44a3b096fdece30f40bf33ee1
zmigd <zmigdalovich@gmail.com>
2025-09-07 18:56:03 +0300

    add modern-normalize

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// import 'modern-normalize'; 
+import 'modern-normalize'; 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
