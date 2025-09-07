2389c673227a6a68cdfe06554f1c44209b6f9129
zmigd <zmigdalovich@gmail.com>
2025-09-07 19:13:08 +0300

    change stucture

--- src/components/APP/App.tsx
+++ src/components/APP/App.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 import css from './App.module.css';
-import CafeInfo from './components/CafeInfo/CafeInfo';
-import VoteOptions from './components/VoteOptions/VoteOptions';
-import VoteStats from './components/VoteStats/VoteStats';
-import Notification from './components/Notification/Notification';
+import CafeInfo from '../CafeInfo/CafeInfo';
+import VoteOptions from '../VoteOptions/VoteOptions';
+import VoteStats from '../VoteStats/VoteStats';
+import Notification from '../Notification/Notification';
 
-import type { Votes, VoteType } from './components/types/votes';
+import type { Votes, VoteType } from '../types/votes';
 
 function App() {
   const [votes, setVotes] = useState<Votes>({
